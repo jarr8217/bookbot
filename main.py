c562df2ae1533df046c0aa19a0ef66063941af1a
Jose Refoyo Ron <joserefoyo93@gmail.com>
2025-08-26 10:37:05 -0400

    feat: added function in stats named get _sorted_char_list  to sort with in a dictionar the word and character count

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from stats import get_num_words
 from utils import get_book_text
-from stats import get_num_characters
+from stats import count_characters
 
 
 def main():
     num_words = get_num_words("frankenstein")
     print(f"{num_words} words found in the document")
 
-    num_characters = get_num_characters("frankenstein")
-    print(f"{num_characters} characters found in the document")
+    book_text = get_book_text("frankenstein")
+    char_counts = count_characters(book_text)
+    print(f"Character counts found in the document: {char_counts}")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -7,16 +7,7 @@ def get_num_words(book_title):
     return len(words)
 
 
-def get_num_characters(book_title):
-    text = get_book_text(book_title)
-
-    return len(text)
-
-
 def count_characters(text):
-    """
-    Returns a dictionary with the count of each character (case-insensitive) in the given text.
-    """
     char_counts = {}
     for char in text.lower():
         if char in char_counts:
