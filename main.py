ece719e0bfa66578330279ffb97129ebd6df6bfb
Jose Refoyo Ron <joserefoyo93@gmail.com>
2025-08-26 10:26:18 -0400

    feat: added function for counting characters within the books

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from stats import get_num_words
 from utils import get_book_text
+from stats import get_num_characters
 
 
 def main():
-    book_text = get_book_text("frankenstein")
-    num_words = get_num_words(book_text)
+    num_words = get_num_words("frankenstein")
     print(f"{num_words} words found in the document")
+
+    num_characters = get_num_characters("frankenstein")
+    print(f"{num_characters} characters found in the document")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -5,3 +5,22 @@ def get_num_words(book_title):
     text = get_book_text(book_title)
     words = text.split()
     return len(words)
+
+
+def get_num_characters(book_title):
+    text = get_book_text(book_title)
+
+    return len(text)
+
+
+def count_characters(text):
+    """
+    Returns a dictionary with the count of each character (case-insensitive) in the given text.
+    """
+    char_counts = {}
+    for char in text.lower():
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
