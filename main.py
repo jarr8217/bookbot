5ce4e39a3482df5e1fa72da66335244ac84f7b96
Jose Refoyo Ron <joserefoyo93@gmail.com>
2025-08-26 00:23:43 -0400

    Add get_book_text function and main entrypoint to read and print frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('greetings boots')
+def get_book_text(book_title):
+    with open(f'books/{book_title}.txt', 'r', encoding="utf-8") as file:
+        return file.read()
+
+
+def main():
+    book_title = input(
+        "Enter the book title (e.g., 'frankenstein'): ").strip().lower()
+    book_text = get_book_text(book_title)
+    print(book_text)
+
+
+if __name__ == "__main__":
+    main()
